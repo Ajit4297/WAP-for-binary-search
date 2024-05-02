eef3f56ac30debda4462d9ee17ada26316039abf
Ajit4297 <168507427+Ajit4297@users.noreply.github.com>
2024-05-02 10:25:24 +0530

    Add files via upload

---
+++ Untitled21.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+l=[20,30,60,50]
+key=90
+l.sort(reverse=True)
+if key in l:
+    print(l.index(key))
+else:
+    print(-1)
+
+
+# In[ ]:
+
+
+
+
